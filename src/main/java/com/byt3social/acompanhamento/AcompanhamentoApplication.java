a6c840cc7acf7d7e6ab6877838e65e1bc6439d7a
Leandro Pereira <leojgpereira@gmail.com>
2023-10-01 20:55:30 -0400

    Commit inicial

---
+++ src/main/java/com/byt3social/acompanhamento/AcompanhamentoApplication.java
@@ -0,0 +1,13 @@
+package com.byt3social.acompanhamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanhamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byt3social/acompanhamento/AcompanhamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byt3social.acompanhamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanhamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
