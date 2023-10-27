e3b348db25957a06c8194cf8083dfda409161db1
Lucas-Welter <boltwelter@gmail.com>
2023-10-27 00:03:49 -0400

    docs: jacoco e springDoc adicionados

--- src/main/java/com/byt3social/acompanhamento/controllers/AcompanhamentoController.java
+++ src/main/java/com/byt3social/acompanhamento/controllers/AcompanhamentoController.java
@@ -4,6 +4,12 @@ import com.byt3social.acompanhamento.dto.AcompanhamentoDTO;
 import com.byt3social.acompanhamento.models.Acompanhamento;
 import com.byt3social.acompanhamento.models.Arquivo;
 import com.byt3social.acompanhamento.services.AcompanhamentoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,70 +19,91 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
+@RequestMapping("/acompanhamentos")
 public class AcompanhamentoController {
     @Autowired
     private AcompanhamentoService acompanhamentoService;
 
-    @GetMapping("/acompanhamentos")
-    public ResponseEntity consultarAcompanhamentos() {
+    @Operation(summary = "Consultar todos os acompanhamentos")
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso!", content = @Content(schema = @Schema(implementation = Acompanhamento.class)))
+    @ApiResponse(responseCode = "401", description = "Consulta mal-sucedida")
+    @GetMapping
+    public ResponseEntity<List<Acompanhamento>> consultarAcompanhamentos() {
         List<Acompanhamento> acompanhamentos = acompanhamentoService.consultarAcompanhamentos();
-
         return new ResponseEntity<>(acompanhamentos, HttpStatus.OK);
     }
 
-    @GetMapping("/acompanhamentos/{id}")
-    public ResponseEntity consultarAcompanhamento(@PathVariable("id") Integer acompanhamentoID) {
-       Acompanhamento acompanhamento = acompanhamentoService.consultarAcompanhamento(acompanhamentoID);
-
+    @Operation(summary = "Consultar um acompanhamento específico cadastrado")
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso!", content = @Content(schema = @Schema(implementation = Acompanhamento.class)))
+    @ApiResponse(responseCode = "404", description = "Acompanhamento não encontrado")
+    @GetMapping("/{id}")
+    public ResponseEntity<Acompanhamento> consultarAcompanhamento(@PathVariable("id") Integer acompanhamentoID) {
+        Acompanhamento acompanhamento = acompanhamentoService.consultarAcompanhamento(acompanhamentoID);
         return new ResponseEntity<>(acompanhamento, HttpStatus.OK);
     }
 
-    @PostMapping("/acompanhamentos")
-    public ResponseEntity cadastrarAcompanhamento(@RequestBody AcompanhamentoDTO acompanhamentoDTO) {
+    @Operation(summary = "Cadastrar uma acompanhamento")
+    @ApiResponse(responseCode = "201", description = "Acompanhamento cadasatrado com sucesso!")
+    @ApiResponse(responseCode = "400", description = "Cadastro mal-sucedido, verifique os dados")
+    @PostMapping
+    public ResponseEntity<Void> cadastrarAcompanhamento(@RequestBody AcompanhamentoDTO acompanhamentoDTO) {
         acompanhamentoService.cadastrarAcompanhamento(acompanhamentoDTO);
-
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping("/acompanhamentos/{id}")
-    public ResponseEntity atualizarAcompanhamento(@PathVariable("id") Integer acompanhamentoID, @RequestBody AcompanhamentoDTO acompanhamentoDTO) {
+    @Operation(summary = "Atualizar um acompanhamento cadastrado")
+    @ApiResponse(responseCode = "204", description = "Acompanhamento atualizado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Acompanhamento não encontrado")
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizarAcompanhamento(@PathVariable("id") Integer acompanhamentoID, @RequestBody AcompanhamentoDTO acompanhamentoDTO) {
         acompanhamentoService.atualizarAcompanhamento(acompanhamentoID, acompanhamentoDTO);
-
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/acompanhamentos/{id}")
-    public ResponseEntity excluirAcompanhamento(@PathVariable("id") Integer acompanhamentoID) {
+    @Operation(summary = "Deletar um acompanhamento cadastrado")
+    @ApiResponse(responseCode = "200", description = "Acompanhamento deletado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Acompanhamento não encontrado")    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirAcompanhamento(@PathVariable("id") Integer acompanhamentoID) {
         acompanhamentoService.excluirAcompanhamento(acompanhamentoID);
-
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/acompanhamentos/{id}/arquivos")
-    public ResponseEntity salvarArquivoAcompanhamento(@PathVariable("id") Integer acompanhamentoID, @RequestBody MultipartFile arquivo) {
+    @Operation(summary = "Salvar um arquivo em um acompanhamento")
+    @ApiResponse(responseCode = "200", description = "Arquivo salvo com sucesso!", content = @Content(schema = @Schema(implementation = Arquivo.class)))
+    @ApiResponse(responseCode = "400", description = "Cadastro mal-sucedido, verifique os dados")
+    @PostMapping("/{id}/arquivos")
+    public ResponseEntity<Arquivo> salvarArquivoAcompanhamento(@PathVariable("id") Integer acompanhamentoID, @RequestParam("arquivo") MultipartFile arquivo) {
         Arquivo arquivoSubmetido = acompanhamentoService.salvarArquivoAcompanhamento(acompanhamentoID, arquivo);
-
-        return new ResponseEntity(arquivoSubmetido, HttpStatus.OK);
+        return new ResponseEntity<>(arquivoSubmetido, HttpStatus.OK);
     }
 
-    @GetMapping("/acompanhamentos/arquivos/{id}")
-    public ResponseEntity recuperarArquivoAcompanhamento(@PathVariable("id") Integer arquivoID) {
+
+    @Operation(summary = "Recuperar um arquivo de um acompanhamento")
+    @ApiResponse(responseCode = "200", description = "Arquivo recuperado com sucesso!", content = @Content(schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "404", description = "Arquivo não encontrado")
+    @GetMapping("/arquivos/{id}")
+    public ResponseEntity<String> recuperarArquivoAcompanhamento(@PathVariable("id") Integer arquivoID) {
         String urlArquivo = acompanhamentoService.recuperarArquivoAcompanhamento(arquivoID);
-
-        return new ResponseEntity(urlArquivo, HttpStatus.OK);
+        return new ResponseEntity<>(urlArquivo, HttpStatus.OK);
     }
 
-    @DeleteMapping("/acompanhamentos/arquivos/{id}")
-    public ResponseEntity excluirArquivoAcompanhamento(@PathVariable("id") Integer arquivoID) {
+
+    @Operation(summary = "Deletar um arquivo de um acompanhamento")
+    @ApiResponse(responseCode = "200", description = "Arquivo deletado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Arquivo não encontrado")   
+    @DeleteMapping("/arquivos/{id}")
+    public ResponseEntity<Void> excluirArquivoAcompanhamento(@PathVariable("id") Integer arquivoID) {
         acompanhamentoService.excluirArquivoAcompanhamento(arquivoID);
-
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/acompanhamentos/organizacoes")
-    public ResponseEntity buscarAcompanhamentosOrganizacao(@RequestHeader("B3Social-Organizacao") String organizacaoId) {
+    @Operation(summary = "Recuperar acompanhamentos de uma organização")
+    @ApiResponse(responseCode = "200", description = "Acompanhamentos recuperados com sucesso!", content = @Content(schema = @Schema(implementation = Acompanhamento.class)))
+    @ApiResponse(responseCode = "404", description = "Organização não encontrada") 
+    @GetMapping("/organizacoes")
+    public ResponseEntity<List<Acompanhamento>> buscarAcompanhamentosOrganizacao(@RequestHeader("B3Social-Organizacao") String organizacaoId) {
         List<Acompanhamento> acompanhamentos = acompanhamentoService.buscarAcompanhamentos(Integer.valueOf(organizacaoId));
-
-        return new ResponseEntity(acompanhamentos, HttpStatus.OK);
+        return new ResponseEntity<>(acompanhamentos, HttpStatus.OK);
     }
 }
--- src/main/java/com/byt3social/acompanhamento/controllers/IndicadorController.java
+++ src/main/java/com/byt3social/acompanhamento/controllers/IndicadorController.java
@@ -3,6 +3,11 @@ package com.byt3social.acompanhamento.controllers;
 import com.byt3social.acompanhamento.dto.IndicadorDTO;
 import com.byt3social.acompanhamento.models.Indicador;
 import com.byt3social.acompanhamento.repositories.IndicadorRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,26 +17,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/indicadores")
 public class IndicadorController {
     @Autowired
     private IndicadorRepository indicadorRepository;
 
-    @GetMapping("/indicadores")
+    @Operation(summary = "Consultar todos os indicadores")
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso!", 
+        content = @Content(schema = @Schema(implementation = Indicador.class)))
+    @GetMapping
     public ResponseEntity consultarIndicadores() {
         List<Indicador> indicadores = indicadorRepository.findAll();
 
         return new ResponseEntity<>(indicadores, HttpStatus.OK);
     }
 
-    @GetMapping("/indicadores/{id}")
+    @Operation(summary = "Consultar um indicador específico")
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso!", 
+        content = @Content(schema = @Schema(implementation = Indicador.class)))
+    @ApiResponse(responseCode = "404", description = "Indicador não encontrado")
+    @GetMapping("/{id}")
     public ResponseEntity consultarIndicador(@PathVariable("id") Integer indicadorID) {
         Indicador indicador = indicadorRepository.findById(indicadorID).get();
 
         return new ResponseEntity<>(indicador, HttpStatus.OK);
     }
 
+    @Operation(summary = "Cadastrar um indicador")
+    @ApiResponse(responseCode = "201", description = "Indicador cadastrado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Cadastro mal-sucedido, verifique os dados")
     @Transactional
-    @PostMapping("/indicadores")
+    @PostMapping
     public ResponseEntity cadastrarIndicador(@RequestBody IndicadorDTO indicadorDTO) {
         Indicador indicador = new Indicador(indicadorDTO);
         indicadorRepository.save(indicador);
@@ -39,8 +55,11 @@ public class IndicadorController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Atualizar um indicador")
+    @ApiResponse(responseCode = "204", description = "Indicador atualizado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Indicador não encontrado")
     @Transactional
-    @PutMapping("/indicadores/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity atualizarIndicador(@PathVariable("id") Integer indicadorID, @RequestBody IndicadorDTO indicadorDTO) {
         Indicador indicador = indicadorRepository.findById(indicadorID).get();
         indicador.atualizar(indicadorDTO);
@@ -48,7 +67,10 @@ public class IndicadorController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/indicadores/{id}")
+    @Operation(summary = "Excluir um indicador")
+    @ApiResponse(responseCode = "200", description = "Indicador excluído com sucesso")
+    @ApiResponse(responseCode = "404", description = "Indicador não encontrado")
+    @DeleteMapping("/{id}")
     public ResponseEntity excluirIndicador(@PathVariable("id") Integer indicadorID) {
         indicadorRepository.deleteById(indicadorID);
 
